0aa632dcd2b9d77d7f8dee1575e2d37e1c7e2997
Arshak Zaqaryan <arshakzakaryan2004@gmail.com>
2025-06-02 20:46:12 +0400

    add String

---
+++ String/String.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include "String.hpp"
+
+String::String(){
+	m_size = 0;
+	m_data = nullptr;
+}
+
+String::String(const char * src){
+	if(src){
+		m_size = std::strlen(src);
+		m_data = new char[m_size + 1];
+		for(size_t i = 0; i <= m_size; ++i){
+			m_data[i] = src[i];
+		}
+	}
+	else{
+		m_size = 0;
+		m_data = nullptr;
+	}
+}
+
+String::~String(){
+	delete []m_data;
+}
+
+size_t String::Length(){
+	return m_size;
+}
+
+const char * String::Access(){
+	return m_data;
+}
+
+void String::Print(){
+	std::cout<<m_data<<std::endl;
+}
+
+
+
+
+
+
+
+
+
---
+++ String/String.hpp
@@ -0,0 +1,19 @@
+#ifndef STRING_HPP
+#define STRING_HPP	
+
+class String{
+public:
+	String();		// Default Constructor
+	String(const char *);	// Parameterized Constructor
+
+	~String();
+	
+	size_t Length();
+	const char * Access();
+	void Print();
+
+private:
+	char * m_data;
+	size_t m_size;
+
+};
